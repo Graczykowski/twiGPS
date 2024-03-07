063f99045bd9c35814326a02630810fee41ef904
Graczykowski <igor.graczykowski@gmail.com>
2024-03-07 22:32:41 +0100

    stats - freq change to count

--- R/funs.R
+++ R/funs.R
@@ -39,8 +39,8 @@ rast_stats = function(raster, stats){
     if (statistic == "range"){
       range = terra::global(raster, fun = statistic, na.rm = TRUE)
       vals = append(vals, range[,2] - range[,1])
-    } else if (statistic == "freq"){
-      freq = raster |> terra::freq() |> dplyr::summarise(n = sum(count)) |>
+    } else if (statistic == "count"){
+      count = raster |> terra::freq() |> dplyr::summarise(n = sum(count)) |>
         as.integer()
       vals = append(vals, freq)
     } else if (statistic == "area") {
