4922e4cd1bd15e8dfd596d5d5593f1ad71835ae5
Graczykowski <igor.graczykowski@gmail.com>
2024-03-09 18:10:26 +0100

    KDE_exposure in CRS in meter and degree, other KDE method as KDE_slow

--- R/KDE_exposure.R
+++ R/KDE_exposure.R
@@ -1,8 +1,6 @@
-#' Kernel Density Estimation exposure
-#'
+#' Kernel Density Estimation in Projected Coordinate Systems
 #' @description
-#' working in all projections but slow
-#'
+#' Code working fast with results to check, working well in projected coordinate systems, WGS84 yet to do
 #'
 #' @param x data frame with lon lat coordinate columns
 #' @param day string in date format compatible with date column in x
@@ -24,11 +22,15 @@
 #' @export
 
 
-KDE_exposure = function(x, day=NULL, cellsize=100, bandwidth = 200, env_data=NULL,
-                        normalize = FALSE, data_extent = NULL, # TODO extent
-                        start_crs = "WGS84", end_crs=NULL, stats=NULL,
-                        act_and_env=FALSE){ # TODO act_and_env
 
+
+
+KDE_proj = function(x, day=NULL, cellsize=100, bandwidth = 200, env_data=NULL,
+                    normalize = FALSE, data_extent = NULL, # TODO extent
+                    start_crs = "WGS84", end_crs=NULL, stats=NULL,
+                    act_and_env=FALSE){ # TODO act_and_env
+
+  buff_const = 0.05 #percent of extent as bbox
 
   x_proj = start_processing(x, day, env_data, data_extent, start_crs, end_crs)
 
@@ -37,55 +39,92 @@ KDE_exposure = function(x, day=NULL, cellsize=100, bandwidth = 200, env_data=NUL
     env_data_proj = terra::project(env_data, terra::crs(x_proj))
   }
 
+  # get extent
+  extent = terra::ext(x_proj)
+
+  # buffer around extent
+  x_const_kde = (terra::xmax(extent) - terra::xmin(extent)) * buff_const
+  y_const_kde = (terra::ymax(extent) - terra::ymin(extent)) * buff_const
+
+  # new extent
+  new_extent = c(terra::xmin(extent) - x_const_kde, terra::xmax(extent) + x_const_kde,
+                 terra::ymin(extent) - y_const_kde, terra::ymax(extent) + y_const_kde)
+  #TODO small cellsize disproportion
+
+
   if (is.numeric(cellsize) & cellsize > 0) { # cellsize included
 
-    grid_rast = terra::rast(crs = terra::crs(x_proj), extent = terra::ext(x_proj),
-                             resolution = cellsize)
+    grid_rast = terra::rast(crs = terra::crs(x_proj))
+    terra::ext(grid_rast) = new_extent # ext before cellsize to avoid cellsize disproportion
+    terra::res(grid_rast) = cellsize #TODO small cellsize disproportion
   } else if (!is.null(env_data)){ #if incorrect cellsize and env_data exists
 
     grid_rast = env_data_proj
-    terra::ext(grid_rast) = terra::ext(x_proj)
+    terra::ext(grid_rast) = new_extent # ext before cellsize to avoid cellsize disproportion
+    terra::res(grid_rast) = terra::res(env_data_proj)
   }
 
+  # coordinate seq
+  x_seq = seq(new_extent[1], new_extent[2], length.out = terra::ncol(grid_rast))
+  y_seq = seq(new_extent[3], new_extent[4], length.out = terra::nrow(grid_rast))
+
+  # coords of every cell
+  expand_grid = expand.grid(x_seq,y_seq)
+
+  # point coords
+  coords = terra::geom(x_proj)[,3:4]
+
+  # kde with sqrt bandwidth
+  kde_data = TDA::kde(coords, Grid = expand_grid, h = sqrt(bandwidth))
+
+  # params for empty raster
+  len_x <- length(x_seq)
+  len_y <- length(y_seq)
+
+  # x, y limits
+  x_min = min(x_seq)
+  x_max = max(x_seq)
+  y_min = min(y_seq)
+  y_max = max(y_seq)
+
+  # empty rast for kde
+  kde_rast = terra::rast(nrows=len_y, ncols=len_x, xmin=x_min, xmax=x_max, ymin=y_min,
+                  ymax=y_max, crs = terra::crs(x_proj))
 
 
-  spat_kde_rast = SpatialKDE::kde(sf::st_as_sf(x_proj), band_width = bandwidth,
-                                  grid = raster::raster(grid_rast)) #cant use terra rast
-  if (normalize == TRUE){
-
-    spat_kde_rast = terra::subst(spat_kde_rast, from = NA, to = 0) # proper range for normalization
-    rast_minmax = terra::minmax(spat_kde_rast) # minmax
-    # calculate normalization to 0-1 range
-    spat_kde_rast = (spat_kde_rast - rast_minmax[1,])/(rast_minmax[2,]-rast_minmax[1,])
-    spat_kde_rast = terra::subst(spat_kde_rast, from = 0, to = NA) # insert NA
-
+  if (normalize == TRUE){ # normalize kde values
+    kde_vals = BBmisc::normalize(as.vector(kde_data), method = "range", range = c(0, 1),
+                                 margin = 1L, on.constant = "quiet")
+  } else {
+    kde_vals = kde_data
   }
 
+  # insert kde values to raster
+  terra::values(kde_rast) = kde_vals
+  kde_rast = terra::subst(kde_rast, from = 0, to = NA)
+  kde_rast = terra::flip(kde_rast, direction='vertical') # flip raster
 
-  spat_kde_rast[spat_kde_rast == 0] = NA
 
-
-  spat_kde_rast = terra::rast(spat_kde_rast)
 
   if (!is.null(env_data)){ # calculate exposure
-    env_data_resamp = terra::resample(env_data_proj, spat_kde_rast)
-    rast_env_kde = spat_kde_rast * env_data_resamp
-
-
+    # only for one point data check
+    # env_max = terra::minmax(env_data)[2]
+    # env_data_proj[env_data_proj > env_max] = NA
+    ##
+    env_data_resamp = terra::resample(env_data_proj, kde_rast)
+    ##
+    # env_data_resamp[env_data_resamp > env_max] = NA
+    ##
+    kde_env_rast = kde_rast * env_data_resamp
 
 
     # calculate env output
-    output = output_calc(act_rast = spat_kde_rast, env_rast = rast_env_kde, stats = stats)
+    output = output_calc(kde_rast, env_rast = kde_env_rast, stats = stats)
   } else {
     # calculate activity output
-    output = output_calc(act_rast = spat_kde_rast, stats = stats)
+    output = output_calc(kde_rast, stats = stats)
   }
-
 
   return(output)
 
-
-
-
 }
-
