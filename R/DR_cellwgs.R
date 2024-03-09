c3cbb4768797ad8a6d80e4e64d0f058c5383682f
Graczykowski <igor.graczykowski@gmail.com>
2024-03-09 17:07:05 +0100

    Combined DR projected and WGS with square cells, first test

--- R/DR_cellwgs.R
+++ R/DR_cellwgs.R
@@ -6,11 +6,13 @@
 #'
 #' @param x data frame with lon lat coordinate columns
 #' @param day string in date format compatible with date column in x
-#' @param cellsize size of raster cell in degrees
-#' @param smoothing smoothing parameter
+#' @param cellsize size of raster cell in meters
+#' @param bandwidth bandwidth in units of x
 #' @param env_data SparRaster object of envirinmental data
 #' @param normalize argument if activity data should be normalized to 0-1 values range
 #' @param data_extent TODO
+#' @param start_crs coordinate system of coordinates in x data frame
+#' @param end_crs coordinate system of output
 #' @param stats statistics calculated
 #' @param act_and_env TODO
 #'
@@ -21,14 +23,14 @@
 #' @export
 
 
-DR_cellwgs = function(x, day=NULL, cellsize=100, smoothing = 0.0007,
-                       normalize = TRUE, env_data=NULL,
-                       data_extent = NULL, # TODO extent
-                       stats=NULL, act_and_env=FALSE){ # TODO act_and_env
+DR_cellwgs = function(x, day=NULL, cellsize=100, bandwidth = 200, env_data=NULL,
+                      normalize = FALSE, data_extent = NULL, # TODO extent
+                      start_crs = "WGS84", end_crs=NULL, stats=NULL,
+                      act_and_env=FALSE){ # TODO act_and_env
 
   buff_const = 0.05 #percent of extent as bbox
 
-  x_proj = start_processing(x, day, env_data, data_extent, "WGS84", "WGS84")
+  x_proj = start_processing(x, day, env_data, data_extent, start_crs, end_crs)
 
 
   # get extent
@@ -43,24 +45,52 @@ DR_cellwgs = function(x, day=NULL, cellsize=100, smoothing = 0.0007,
                  terra::ymin(extent) - y_const_dr, terra::ymax(extent) + y_const_dr)
 
 
+  if (!is.null(env_data)){ # change env_data crs beforehand
+    env_data_proj = terra::project(env_data, terra::crs(x_proj))
+  }
 
 
-  grid_rast = terra::rast(crs = terra::crs(x_proj))
-  terra::ext(grid_rast) = new_extent # ext before cellsize to avoid cellsize disproportion
-  terra::res(grid_rast) = cellsize
+
+  if (is.numeric(cellsize) & cellsize > 0) { # cellsize included
+
+    if (terra::linearUnits(x_proj) == 0){ # crs units in degrees
+      dist_lon = geosphere::distm(c(new_extent[1], new_extent[3]), c(new_extent[2], new_extent[3]),
+                                  fun = geosphere::distHaversine)
+      dist_lat = geosphere::distm(c(new_extent[1], new_extent[3]), c(new_extent[1], new_extent[4]),
+                                  fun = geosphere::distHaversine)
+
+      x_cells = (dist_lon / cellsize) |> as.integer()
+
+      y_cells = (dist_lat / cellsize) |> as.integer()
+
+    } else {
+      grid_rast = terra::rast(crs = terra::crs(x_proj))
+      terra::ext(grid_rast) = new_extent # ext before cellsize to avoid cellsize disproportion
+      terra::res(grid_rast) = cellsize #TODO small cellsize disproportion
+    }
+
+  } else if (!is.null(env_data)){ #if incorrect cellsize and env_data exists
+
+    grid_rast = env_data_proj
+    terra::ext(grid_rast) = new_extent # ext before cellsize to avoid cellsize disproportion
+    terra::res(grid_rast) = terra::res(env_data_proj)
+  }
+
+
 
 
   # point coords
   coords = terra::geom(x_proj)[,3:4]
 
   # number of cells
-  x_cells = terra::ncol(grid_rast)
-  y_cells = terra::nrow(grid_rast)
-
+  if (exists(grid_rast)){
+    x_cells = terra::ncol(grid_rast)
+    y_cells = terra::nrow(grid_rast)
+  }
 
 
   # calculate DR data
-  dr_data = DR_simple(coords, kernel= "Gaussian", h = smoothing,
+  dr_data = DR_simple(coords, kernel= "Gaussian", h = bandwidth,
                       x_res=x_cells, y_res=y_cells, xlim=new_extent[1:2],
                       ylim=new_extent[3:4])
 
@@ -74,8 +104,8 @@ DR_cellwgs = function(x, day=NULL, cellsize=100, smoothing = 0.0007,
   lat_min = min(dr_data$y_grid)
   lat_max = max(dr_data$y_grid)
 
-  spat_dr_rast = terra::rast(nrows=len_lat, ncols=len_lon, xmin=lon_min, xmax=lon_max,
-                             ymin=lat_min, ymax=lat_max)
+  dr_rast = terra::rast(nrows=len_lat, ncols=len_lon, xmin=lon_min, xmax=lon_max,
+                        ymin=lat_min, ymax=lat_max, crs = terra::crs(x_proj))
 
   # insert KDE values to raster
   if (normalize == TRUE){
@@ -84,28 +114,27 @@ DR_cellwgs = function(x, day=NULL, cellsize=100, smoothing = 0.0007,
   } else {
     dr_values = dr_data$gr_alpha
   }
-  terra::values(spat_dr_rast) = dr_values
-  spat_dr_rast[spat_dr_rast == 0] = NA # NA vals
-  spat_dr_rast = terra::flip(spat_dr_rast, direction='vertical') # flip raster
+  terra::values(dr_rast) = dr_values
+  dr_rast[dr_rast == 0] = NA # NA vals
+  dr_rast = terra::flip(dr_rast, direction='vertical') # flip raster
 
 
   if (!is.null(env_data)){ # calculate exposure
-    env_data_proj = terra::project(env_data, spat_dr_rast)
     # only for one point data check
-    env_max = terra::minmax(env_data)[2]
-    env_data_proj[env_data_proj > env_max] = NA
+    # env_max = terra::minmax(env_data)[2]
+    # env_data_proj[env_data_proj > env_max] = NA
     ##
-    env_data_resamp = terra::resample(env_data_proj, spat_dr_rast)
+    env_data_resamp = terra::resample(env_data_proj, dr_rast)
     ##
-    env_data_resamp[env_data_resamp > env_max] = NA
+    # env_data_resamp[env_data_resamp > env_max] = NA
     ##
-    rast_env_dr = spat_dr_rast * env_data_resamp
+    rast_env_dr = dr_rast * env_data_resamp
 
     # calculate env output
-    output = output_calc(spat_dr_rast, env_rast = rast_env_dr, stats = stats)
+    output = output_calc(dr_rast, env_rast = rast_env_dr, stats = stats)
   } else {
     # calculate activity output
-    output = output_calc(spat_dr_rast, stats = stats)
+    output = output_calc(dr_rast, stats = stats)
 
   }
 
